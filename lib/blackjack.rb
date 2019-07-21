2121f89fd9f1590dddbcd7bb1cdf9371caa25473
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-21 05:29:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,13 +65,13 @@ def runner
   
   welcome 
   random_num = initial_round
-  while random_num < 21 do  
-    prompt_user
-    get_user_input
-      hit?(random_num)
+#   while random_num < 21 do  
+#     prompt_user
+#     get_user_input
+#       hit?(random_num)
      
       
-      display_card_total(random_num)
-end
+#       display_card_total(random_num)
+# end
 end
     
