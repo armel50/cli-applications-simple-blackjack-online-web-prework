20d2c83349ba88e1ace20ec9ef6f269f08452bf1
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-21 04:52:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,14 +44,14 @@ def hit?(current)
   end
   if input != "h" && input != "s" 
     invalid_command
+    prompt_user
   end
 end
 
 def invalid_command
   # code invalid_command here
   puts  "Please enter a valid command"
-  prompt_user
-  get_user_input
+  
 end
 
 #####################################################
